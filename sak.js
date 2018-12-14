b838bf21d0a5b97fba3f366c7d0878fcf969e2b4
coel16 <modopojken@gmail.com>
2018-12-14 10:35:56 +0100

    Git commit cyka

---
+++ sak.js
@@ -0,0 +1,65 @@
+let colorsHash = [];
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+colorsHash["255000000"] = "red";
+
+function draw() {
+
+  canvas.width  = window.innerWidth-9;
+  canvas.height = window.innerHeight-10;
+
+  let bgImg = document.getElementById("bg-gfx");
+ 
+  ctx.font = '48px serif';
+  ctx.fillStyle = '#00FF00';
+  ctx.fillText('JulQuizen!',650,50);
+
+//
+//
+//
+//   Läs nu!!!!!!!         https://lavrton.com/hit-region-detection-for-html5-canvas-and-how-to-listen-to-click-events-on-canvas-shapes-815034d7e9f8/
+//
+//
+//
+
+
+
+
+
+
+  ctx.fillStyle = "black"
+  ctx.fillRect(463,164,602,102);
+  ctx.fillRect(622,299,252,77);
+  ctx.fillRect(622,399,252,77);
+  ctx.fillRect(622,499,252,77);
+  ctx.fillStyle = "green";
+  ctx.fillRect(464,165,600,100);
+  ctx.fillRect(623,300,250,75);
+  ctx.fillRect(623,400,250,75);
+  ctx.fillRect(623,500,250,75);
+  ctx.fillStyle = "red";
+  ctx.font = "30px serif";
+  ctx.fillText("Här kommer frågorna att visas!",480,225);
+  ctx.font = "25px serif";
+  ctx.fillText("Här är ett Alternativ!",630,345);
+  ctx.fillText("Här är ett Alternativ!",630,445);
+  ctx.fillText("Här är ett Alternativ!",630,545);
+
+
+
+
+  window.requestAnimationFrame(draw);
+}
+document.getElementById("canvas").addEventListener("click", function(e) {
+    let pixel = ctx.getImageData(e.x, e.y, 1, 1).data;
+    let color = `rgb(${pixel[0]},${pixel[1]},${pixel[2]})`;
+    let shape = colorsHash[color];
+    if (shape) {
+        console.log('click on: ' + shape);
+    }
+    console.log(e.x, e.y, pixel);
+}, false);
+
+
+
+window.requestAnimationFrame(draw);
--- test.js
+++ test.js
@@ -3,28 +3,9 @@ function draw() {
 	if (canvas.getContext) {
 		var ctx = canvas.getContext('2d');
 
-
-
-    ctx.beginPath();
-    ctx.arc(175, 175, 150, 0, Math.PI * 2, true); // Outer circle
-    ctx.moveTo(39, 175);
-    ctx.arc(175, 175, 135, 0, Math.PI, false);  // Mouth (clockwise)
-    ctx.moveTo(615, 165);
-    ctx.arc(160, 150, 15, 0, Math.PI * 2, true);  // Left eye
-    ctx.moveTo(195, 165);
-    ctx.arc(190, 140, 15, 0, Math.PI * 2, true);  // Right eye
-    ctx.stroke();
-
-
-    ctx.beginPath();
-    ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
-    ctx.moveTo(110, 75);
-    ctx.arc(75, 75, 35, 0, Math.PI, false);  // Mouth (clockwise)
-    ctx.moveTo(65, 65);
-    ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // Left eye
-    ctx.moveTo(95, 65);
-    ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // Right eye
-    ctx.stroke();
+    ctx.font = '48px serif';
+    ctx.textBaseline = '';
+    ctx.strokeText('Hello World', 10, 50);
 
 
 
@@ -39,5 +20,6 @@ function draw() {
 		//ctx.strokeRect(50,50,50,50);
 	}
 }
+
 
 document.onload = draw();
